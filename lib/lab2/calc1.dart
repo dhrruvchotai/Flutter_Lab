8848d5f4fd367425ee552b5bf296372f76054aaa
dhrruvchotai <23010101051@darshan.ac.in>
2024-12-03 11:32:57 +0530

    added lab 4

--- lib/lab2/calc1.dart
+++ lib/lab2/calc1.dart
@@ -1,29 +1,47 @@
 import "dart:io";
 
 void main(){
-  double ans = 0;
   print("Enter a string : ");
   String str = stdin.readLineSync()!;
 
-  List ch = [];
-  List nums = [];
+  List<String> elmts = str.split(" ");
 
-  for(int i=0;i<str.length;i++){
-    if(str[i] == '+'){
-      ch.add('+');
+  for(int i=0;i<elmts.length;i++){
+    while(elmts.contains('/')){
+      int indx = elmts.lastIndexOf("/");
+      int a = int.parse(elmts[indx-1]);
+      int b = int.parse(elmts[indx+1]);
+      int ans = a~/b;
+      elmts.removeRange(indx-1,indx+2);
+      elmts.insert(indx - 1, ans.toString());
     }
-    else if(str[i] == '-'){
-      ch.add('-');
+    while(elmts.contains("*")){
+      int indx = elmts.lastIndexOf("*");
+      int a = int.parse(elmts[indx-1]);
+      int b = int.parse(elmts[indx+1]);
+      int ans = a*b;
+      elmts.removeRange(indx-1,indx+2);
+      elmts.insert(indx - 1, ans.toString());
     }
-    else if(str[i] == '*'){
-      ch.add('-');
+    while(elmts.contains("+")){
+      int indx = elmts.lastIndexOf("+");
+      int a = int.parse(elmts[indx-1]);
+      int b = int.parse(elmts[indx+1]);
+      int ans = a+b;
+      elmts.removeRange(indx-1,indx+2);
+      elmts.insert(indx - 1, ans.toString());
     }
-    if(str[i] == '/'){
-      ch.add('/');
+    while(elmts.contains("-")){
+      int indx = elmts.lastIndexOf("-");
+      int a = int.parse(elmts[indx-1]);
+      int b = int.parse(elmts[indx+1]);
+      int ans = a-b;
+      elmts.removeRange(indx-1,indx+2);
+      elmts.insert(indx - 1, ans.toString());
     }
-
   }
 
-  print(ch);
+  print(elmts[0]);
 
 }
+
