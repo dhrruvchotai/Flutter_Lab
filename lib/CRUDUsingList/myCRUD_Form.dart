d324c735bd40b0e32a6abe6804f539677008e4a3
dhrruvchotai <23010101051@darshan.ac.in>
2024-12-30 20:06:16 +0530

    added CRUD using form

---
+++ lib/CRUDUsingList/myCRUD_Form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class myCRUD_Form extends StatelessWidget {
+  const myCRUD_Form({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CRUD Using Form'),backgroundColor: Colors.blue,),
+
+    );
+  }
+}
